3413a746cac6db451e2f9c7da62a22efd2877c74
Victor Ferrer <vikton@aules-111-208.udl.net>
2017-03-04 11:16:50 +0100

    Class Book

---
+++ bookoftheday.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf8 :
+
+"""
+Client web per https://www.packtpub.com/packt/offers/free-learning
+
+@author: victor.ferrer.2012@gmail.com
+"""
+
+
+class BookOfTheDay(object):
+    """Book Class."""
+
+    def main(self):
+        """Main de la funció."""
+
+
+if __name__ == "__main__":
+    pass
